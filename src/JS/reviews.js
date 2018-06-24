eb44c41b28d750d0cebec6b16bf25acc71742d25
KyleRusscher <russchky@mail.gvsu.edu>
2018-06-24 15:32:35 -0400

    .

--- src/JS/reviews.js
+++ src/JS/reviews.js
@@ -11,3 +11,12 @@ setInterval(function(){
 	if(number >= 5) number = 0;
 }, 5000);
 
+
+function formSubmit(){
+	var nL = document.querySelectorAll(".normalInput");
+	let data = Array.from(nL)
+	var newD = data.map(line => line.value);
+	console.log(`Name: ${newD[0]}. Message: ${newD[3]} Phone Number: ${newD[1]} Email: ${newD[2]}`)
+}
+
+
