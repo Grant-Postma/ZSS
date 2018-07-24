7ae73ebb47a46da0a8f63903432d4a67d170b1b4
KyleRusscher <russchky@mail.gvsu.edu>
2018-07-23 21:29:22 -0400

    .

--- src/JS/reviews.js
+++ src/JS/reviews.js
@@ -1,5 +1,4 @@
-
-
+"use strict";
 
 /*
 setInterval(function(){
@@ -14,24 +13,15 @@ setInterval(function(){
 
 */
 
-	
-
-
-
-
-
-
-
-
 var inputSummoner = document.getElementById("dInput");
 
 var url_string = window.location.href;
 //console.log(`1st url ${url_string}`)
 var url = new URL(url_string);
-var input= {};
+var input = {};
 input.path = [{}];
 input.path[0].id = url.searchParams.get("size");
-if(input.path[0].id) changeStuff(input);
+if (input.path[0].id) changeStuff(input);
 
 inputSummoner.addEventListener("keyup", function (event) {
 	// Cancel the default action, if needed
@@ -41,17 +31,17 @@ inputSummoner.addEventListener("keyup", function (event) {
 		// Trigger the button element with a click
 
 
-		location.href = `http://maps.google.com/maps?saddr=”${dInput.value}”&daddr=”231 N Centennial St, Zeeland, MI 49464”`
-
+		location.href = "http://maps.google.com/maps?saddr=”" + dInput.value + "”&daddr=”231 N Centennial St, Zeeland, MI 49464”";
 	}
 });
 
-
 function formSubmit() {
 	var nL = document.querySelectorAll(".normalInput");
-	let data = Array.from(nL)
-	var newD = data.map(line => line.value);
-	console.log(`Name: ${newD[0]}. Message: ${newD[3]} Phone Number: ${newD[1]} Email: ${newD[2]}`)
+	var data = Array.from(nL);
+	var newD = data.map(function (line) {
+		return line.value;
+	});
+	console.log("Name: " + newD[0] + ". Message: " + newD[3] + " Phone Number: " + newD[1] + " Email: " + newD[2]);
 }
 
 function menu() {
@@ -66,63 +56,54 @@ function off() {
 	menu.style.width = '0px';
 }
 
-
+$("h4").css({ "color": "green" });
 
 setTimeout(function () {
-	$('#talkP, #letsTalk, #flex-words p, #flex-words a, .box1, #flex-words h1, #flex-words h2, #flex-words h3, #flex-words h4').css({ 'opacity': '1', 'transform': 'translateX(0px)' })
-	$('#color').css({ 'transform': 'translateY(0px)' })
+	$('#talkP, #letsTalk, #flex-words p, #flex-words a, .box1, #flex-words h1, #flex-words h2, #flex-words h3, #flex-words h4').css({ 'opacity': '1', 'transform': 'translateX(0px)' });
+	$('#color').css({ 'transform': 'translateY(0px)' });
 }, 0);
 
-
 setTimeout(function () {
-	$(' #find-us, .pure-group, .text-align, .corner-left, .pure-button').css({ 'opacity': '1', 'transform': 'translateX(0px)' })
+	$(' #find-us, .pure-group, .text-align, .corner-left, .pure-button').css({ 'opacity': '1', 'transform': 'translateX(0px)' });
 }, 300);
 //home page unti sizing
 
 setTimeout(function () {
-	$('#unit1').css({ 'transform': 'translateY(0px)' })
+	$('#unit1').css({ 'transform': 'translateY(0px)' });
 }, 0);
 
 setTimeout(function () {
-	$('#unit2').css({ 'transform': 'translateY(0px)' })
+	$('#unit2').css({ 'transform': 'translateY(0px)' });
 }, 200);
 
 setTimeout(function () {
-	$('#unit3').css({ 'transform': 'translateY(0px)' })
+	$('#unit3').css({ 'transform': 'translateY(0px)' });
 }, 400);
 
 setTimeout(function () {
-	$('.contact-dividor').css({ 'opacity': '1', 'transform': 'translateX(0px)' })
+	$('.contact-dividor').css({ 'opacity': '1', 'transform': 'translateX(0px)' });
 }, 500);
-
 
 function changeStuff(event) {
 	var pageWidth = $(window).width();
 	var tab;
-	if(event.path) tab = event.path[0].id;
-	else tab = event.currentTarget.id;
+	if (event.path) tab = event.path[0].id;else tab = event.currentTarget.id;
 	$(".sizeBody").removeClass("active");
 	$('#sizeHead div').removeClass("on");
-	$(`#n${tab}`).addClass("active");
-	$(`#${tab}`).addClass("on");
+	$("#n" + tab).addClass("active");
+	$("#" + tab).addClass("on");
 	var size = document.getElementsByClassName('on')[0].innerHTML;
-	$('.dropbtn').html(`Unit Sizes - ${size} <span class="menu">menu </span> <i class="arrow fas fa-angle-double-down"></i>`)
+	$('.dropbtn').html("Unit Sizes - " + size + " <span class=\"menu\">menu </span> <i class=\"arrow fas fa-angle-double-down\"></i>");
 	if (pageWidth < 768) {
-		$('#sizeHead').css('display', 'none')
+		$('#sizeHead').css('display', 'none');
 	}
-
 }
 
-
-const headNames = document.querySelectorAll('#sizeHead div');
-headNames.forEach(name => name.addEventListener('click', changeStuff));
-
+var headNames = document.querySelectorAll('#sizeHead div');
+headNames.forEach(function (name) {
+	return name.addEventListener('click', changeStuff);
+});
 
 function expand() {
 	$('#sizeHead').toggle();
 }
-
-
-
-
-
